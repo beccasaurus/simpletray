5cff40a95df9b6499015acb9e43ddbd0c680bae3
remi <remi@remitaylor.com>
2008-09-24 23:59:37 -0700

    little working example of grabbing the methods i wanna catch and figured out how plugins will be written, etc

--- testing/quicktest.rb
+++ testing/quicktest.rb
@@ -1,7 +1,39 @@
 #! /usr/bin/env ruby
 
+class SimpleTray
+  def self.menu title, &block
+    puts "title: #{title}"
+    App.new.instance_eval &block
+  end
+  
+  # PLUGINS SHOULD EXTEND THIS
+  class App
+    def initialize
+    end
+    def item *args
+      puts "called ITEM with #{args.inspect}"
+    end
+    def method_missing method, *args, &block
+      puts "App.method_missing for #{method.inspect} with #{args.inspect} and block: #{block.inspect}"
+    end
+  end
+end
+
 SimpleTray.menu 'Title' do
   
+  about { alert "this is my tray app!" }
+  _____
+
+  item 'My Name' do 
+  end
+
+  item 'blah', lambda {}
+
+  menu do 
+    # ...
+  end
+
+=begin
   my_tickets.click {}
   my_tickets.items {}
 
@@ -20,7 +52,6 @@ SimpleTray.menu 'Title' do
     neato   { puts "blah!"        }
   end
 
-  about { alert "this is my tray app!" }
 
   seperator
 
@@ -29,5 +60,6 @@ SimpleTray.menu 'Title' do
   ________
 
   # ^ if __ >= 2 chars, insert seperator
+=end
 
 end
