c5cdc93c63b16f1e19a7e368a55ce25a1c2b7201
remi <remi@remitaylor.com>
2008-09-26 01:16:20 -0700

    first working version!  pushing up (github should make a gem).

--- testing/simpletray-app.rb
+++
@@ -1,9 +0,0 @@
-# an example system tray app using simpletray
-require 'simpletray'
-
-SimpleTray.app 'My App' do {
-
-  'First' => lambda { puts "CLICKED FIRST" },
-  'Second' => lambda { puts "CLICKED SECOND" }
-
-} end
