379f13ad2eef9acbac0e32d8d1c5b9faef1fbb2d
remi <remi@remitaylor.com>
2008-09-26 00:02:48 -0700

    submenus seem to be working (atleast 1 level deep, haven't tried more yet)

--- testing/quicktest.rb
+++ testing/quicktest.rb
@@ -7,29 +7,6 @@ def msgbox msg
   Wx::MessageDialog.new(@frame, msg, 'Ashacache', Wx::OK, Wx::DEFAULT_POSITION ).show_modal
 end
 
-class DynamicMenuItem < Wx::MenuItem
-  def initialize *args
-    super *args
-    self.get_menu.evt_menu_highlight(self){ |evt| on_highlight(evt) }
-    @menu = Wx::Menu.new
-    self.set_sub_menu @menu
-  end
-  def on_highlight(evt)  end
-end
-
-class MembersMenuItem < DynamicMenuItem
-  def on_highlight(evt)
-    if @menu.get_menu_item_count == 0
-      %w( why hello there ).each_with_index do |x,i|
-        menuitem = Wx::MenuItem.new @menu, -1, x
-        @menu.append_item menuitem
-        @menu.evt_menu(menuitem) { msgbox("clicked on item: #{x}") }
-        ## @menu.append_item MemberMenuItem.new(@menu, 8000 + i, x)
-      end
-    end
-  end
-end
-
 ### SimpleTray
 
 class SimpleTray
@@ -39,6 +16,54 @@ class SimpleTray
   end
   def self.run &block
     Wx::App.run &block
+  end
+
+  class MenuItem < Wx::MenuItem
+    def initialize menu, name, icon = nil, &block
+      super(menu, -1, name)
+      @menu = menu
+      @item_block = block
+      self.get_menu.evt_menu_highlight(self){ |evt| on_highlight(evt) }
+      @menu = Wx::Menu.new
+      self.set_sub_menu @menu
+    end
+    def on_highlight(evt)
+      if @menu.get_menu_item_count == 0
+        instance_eval &@item_block
+      end
+    end
+
+    # everything below here copied (for testing - i will DRY it up)
+    def item name, icon = nil, &block
+      block = icon if block.nil? and icon.respond_to?:call
+      item = Wx::MenuItem.new @menu, -1, name
+      @menu.append_item item
+      @menu.evt_menu(item){ block.call }
+      item
+    end
+    def menu name, icon = nil, &block
+      block = icon if block.nil? and icon.respond_to?:call
+      item = SimpleTray::MenuItem.new @menu, name, icon, &block
+      @menu.append_item item
+    end
+    def seperator
+      @menu.append_separator
+    end
+    def exit name = 'Quit'
+      @menu.append Wx::ID_EXIT, name
+      @menu.evt_menu(Wx::ID_EXIT){ Kernel::exit }
+    end
+    alias quit exit
+    def method_missing method, *args, &block
+      method = method.to_s
+      if method[/^_*$/]
+        seperator
+      elsif method[/^_.*_$/]
+        menu method.titleize.strip, *args, &block
+      else
+        item method.titleize, *args, &block
+      end
+    end
   end
   
   class App < Wx::TaskBarIcon
@@ -60,6 +85,8 @@ class SimpleTray
       instance_eval &@item_block
       @menu
     end
+
+    # all below here is shared ... will DRY up ...
     def item name, icon = nil, &block
       block = icon if block.nil? and icon.respond_to?:call
       item = Wx::MenuItem.new @menu, -1, name
@@ -69,11 +96,11 @@ class SimpleTray
     end
     def menu name, icon = nil, &block
       block = icon if block.nil? and icon.respond_to?:call
-      item = MembersMenuItem.new @menu, 7000, name
+      item = SimpleTray::MenuItem.new @menu, name, icon, &block
       @menu.append_item item
     end
     def seperator
-      puts "seperator"
+      @menu.append_separator
     end
     def exit name = 'Quit'
       @menu.append Wx::ID_EXIT, name
@@ -105,8 +132,10 @@ SimpleTray.app 'My Cool App' do
 
   item 'blah', lambda { puts 'hello from blah' }
 
-  menu 'xxx' do 
-    # ...
+  _menu_ 'xxx' do 
+    menu_item_1 { msgbox "you clicked menu item 1!" }
+    ___
+    menu_item_2 { puts "clicked number 2 !!!!!" }
   end
 
   options { puts "on click" }
