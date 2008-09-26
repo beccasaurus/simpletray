6a8a572d1d3b1b2c3feedfd93dad152cd5b1476b
remi <remi@remitaylor.com>
2008-09-26 02:05:50 -0700

    added an example ... using abunchof dog images  :P

---
+++ examples/dogs/dogs.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/env ruby
+%w( rubygems simpletray ).each {|lib| require lib }
+
+dog_image_numbers = Dir[File.join(File.dirname(__FILE__),'?.png')].sort.map { |x| x.sub(/.*(\d+).png$/,'\1') }
+
+SimpleTray.app 'Dogs' do
+
+  dog_image_numbers.each do |i|
+    item("Dog #{i}", "#{i}.png" ){ msgbox "you clicked on dog ##{i}" }
+  end
+
+  _dogs_ do
+    dog_image_numbers.each do |i|
+      item("Dog #{i}", "#{i}.png" ){ msgbox "you clicked on dog ##{i}" }
+    end
+  end
+  ____
+  exit
+
+end
